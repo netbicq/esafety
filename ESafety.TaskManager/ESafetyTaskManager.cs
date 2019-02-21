339ad7b35073d25534905ab48c131f2ed83997ef
2youge <quickcq@hotmail.com>
2019-02-21 18:03:00 +0800

    Init TaskService

---
+++ ESafety.TaskManager/ESafetyTaskManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESafety.TaskManager
+{
+    partial class ESafetyTaskManager : ServiceBase
+    {
+        public ESafetyTaskManager()
+        {
+            InitializeComponent();
+        }
+
+        
+        protected override void OnStart(string[] args)
+        {
+            // TODO: 在此处添加代码以启动服务。
+        }
+
+        protected override void OnStop()
+        {
+            // TODO: 在此处添加代码以执行停止服务所需的关闭操作。
+        }
+    }
+}
